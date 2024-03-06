a67de65295335464d25d14daf05dd14c45d23248
Joel Dev <joeldavidsolanooerez@gmail.com>
2024-03-06 08:08:41 -0500

    Se agrega el proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Ellas Shoes</title>
+</head>
+<body class="bg-slate-600">
+
+<header class="">
+    <nav class="bg-slate-400 h-10 ">
+
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+        <a href="">Inicio</a>
+        <a href="">Nosotros</a>
+        <a href="">Contacto</a>
+
+
+
+
+        </div>
+
+        
+
+
+
+    
+
+
+
+
+    </nav>
+
+
+
+</header>
+
+
+    
+</body>
+</html>
